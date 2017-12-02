89797492f5b3892ced497f87dcd00a35666fdad4
Angel Recinos <migueln6@gmail.com>
2017-12-01 23:56:26 -0600

    updating skeleton to reflex last changes

--- skeleton/tests/runtests.sh
+++ skeleton/tests/runtests.sh
@@ -1,16 +1,16 @@
 echo "Running unit tests:"
 
-for i in tests/*_tests
+for i in build/tests/*_tests
 	do
 		if test -f $i
 			then
-			if $VALGRIND ./$i 2>> tests/tests.log
+			if $VALGRIND ./$i 2>> build/tests/tests.log
 				then
-				echo $I PASS
+				echo $i PASS
 				else
 					echo "ERROR in test $i: here's thests/tests.log"
 					echo "------"
-					tail tests/tests.log
+					tail build/tests/tests.log
 					exit 1
 				fi
 			fi
